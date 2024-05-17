e42d1416d5e81926a9ca84aa08d7d950d273da3c
calvinNdmb <calvin.ndoumbe@efrei.net>
2024-05-17 11:08:48 +0200

    chore: Add requirements.txt and test.py files

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy as np
+from ultralytics import YOLO
+import cv2
+
+
+im1 = cv2.imread('training_database/3466294_2f5bd421f8_n.jpg')
+
+
+model = YOLO('best.pt')
+results = model.predict(source=[im1])
+
+
+print (type(results))
