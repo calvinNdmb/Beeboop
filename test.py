6bb0c9d6b17f386cd535b29df45a11f7f5f62840
calvinNdmb <calvin.ndoumbe@efrei.net>
2024-05-17 13:13:07 +0200

    chore: Update YOLO model filename in test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import numpy as np
 from ultralytics import YOLO
 import cv2
 im1 = cv2.imread('training_database/3466294_2f5bd421f8_n.jpg')
-model = YOLO('best.pt')
+model = YOLO('best_model.pt')
 results = model.predict(source=[im1])
 print (type(results))
