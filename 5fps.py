48e0ef2c194dcec69b1499e9846d4b9af11e962a
calvinNdmb <calvin.ndoumbe@efrei.net>
2024-07-05 10:22:03 +0200

    Update video file path and output filename in 5fps.py

--- 5fps.py
+++ 5fps.py
@@ -2,7 +2,7 @@ import cv2
 from ultralytics import YOLO, solutions
 
 model = YOLO("models/lil_big_ai.pt")
-cap = cv2.VideoCapture("test_video_input/My Video.mp4")
+cap = cv2.VideoCapture("test_video_input/2024-07-04 22-22-47.mp4")
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
@@ -10,7 +10,7 @@ w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FR
 region_points = [(0, 0), (w, 0), (w, h), (0, h)]
 
 # Video writer
-video_writer = cv2.VideoWriter("object_counting_output.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+video_writer = cv2.VideoWriter("object_counting_output2.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
 # Init Object Counter
 counter = solutions.ObjectCounter(
