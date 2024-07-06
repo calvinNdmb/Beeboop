a6f40009e5714f0991f75f6d942047085ddd16bf
calvinNdmb <calvin.ndoumbe@efrei.net>
2024-07-06 16:37:00 +0200

    Update test.py to use correct image file path and YOLO model filename

--- utilities/test.py
+++ utilities/test.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import cv2
-im1 = cv2.imread('training_database/3466294_2f5bd421f8_n.jpg')
-model = YOLO('best_model.pt')
+im1 = cv2.imread('utilities/test.jpg')
+model = YOLO('models/best.pt')
 results = model.predict(source=[im1])
 print (type(results))
